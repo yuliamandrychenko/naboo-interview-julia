1539f69e09617dc880e558275bd3bd30d5092b34
juliamandrychenko <juliamand.jm@gmail.com>
2026-01-22 12:41:16 +0100

    test

--- e2e/tests/authentication.spec.ts
+++ e2e/tests/authentication.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test'
 
 test.describe('Init', () => {
   test.beforeEach(async ({ page }) => {
-    await page.goto('/')
+    await page.goto('https://www.naboo.app/')
   })
 
   test('should see header', async ({ page }) => {
