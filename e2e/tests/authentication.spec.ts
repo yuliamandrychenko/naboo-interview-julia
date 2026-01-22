22501718f70f77539d3be5219f1a30b910ede9a0
juliamandrychenko <juliamand.jm@gmail.com>
2026-01-22 16:20:02 +0100

    new tests added

--- e2e/tests/authentication.spec.ts
+++ e2e/tests/authentication.spec.ts
@@ -2,10 +2,19 @@ import { test, expect } from '@playwright/test'
 
 test.describe('Init', () => {
   test.beforeEach(async ({ page }) => {
-    await page.goto('https://www.naboo.app/')
+    await page.goto('http://localhost:3001/')
   })
 
-  test('should see header', async ({ page }) => {
+  test('Verification page displays correctly', async ({ page }) => {
     await expect(page.getByTestId(`header`)).toBeVisible()
+    await expect(page.getByRole('heading', { name: 'Accueil' })).toContainText('Accueil')
+    await expect(page.getByRole('heading', { name: 'Découvrez les dernières' })).toContainText('Découvrez les dernières')
+    await expect(page.getByRole('link', { name: 'Candidator' })).toContainText('Candidator')
+    await expect(page.getByRole('link', { name: 'Découvrez des activités' })).toContainText('Découvrez des activités')
+    await expect(page.getByRole('link', { name: 'Explorer' })).toContainText('Explorer')
+    await expect (page.getByRole('button', { name: 'Voir plus' }).first()).toBeVisible()
+    await expect(page.getByText('SPA Saint-MaloSaint-Malo120€/')).toBeVisible()
+    await expect(page.locator('path').nth(2)).toBeVisible()
+
   })
 })
