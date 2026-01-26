25c408094ce158460d90dc6f35c525da911f3e5d
juliamandrychenko <juliamand.jm@gmail.com>
2026-01-26 16:09:37 +0100

    add new tests Explorer

---
+++ e2e/tests/03_Explorer.spec.ts
@@ -0,0 +1,53 @@
+import { test, expect } from '@playwright/test'
+
+
+  // UAT test
+  test('ExplorerUAT', async ({ page }) => {
+    await page.goto('/explorer');
+    //verification if connected 
+    await page.locator('svg').first().click();;
+    await expect(page.getByRole('link', { name: 'Déconnection' })).toBeVisible()
+
+    // verification of explorer page elements
+    await expect(page.getByRole('heading', { name: 'Trouvez une activité dans' })).toBeVisible();
+    await expect(page.getByRole('link', { name: 'Chamonix Lorem ipsum dolor' })).toBeVisible();
+    
+    // go to inital state + verification explorer button
+    await page.getByRole('link', { name: 'Explorer' }).click();
+    await page.waitForURL('/explorer');
+    await page.getByRole('link', { name: 'Candidator' }).click();
+  })
+
+// Test Explorer Details
+  test('ExplorerDetails', async ({ page }) => {
+    await page.goto('/explorer');
+//verification if connected + in the explorer page
+    await page.locator('svg').first().click();;
+    await expect(page.getByRole('link', { name: 'Déconnection' })).toBeVisible();
+    await expect(page.getByRole('heading', { name: 'Trouvez une activité dans' })).toBeVisible();
+
+// Test Explorer Details
+    await page.getByRole('link', { name: 'Chamonix Lorem ipsum dolor' }).click();
+    await expect(page.getByRole('heading', { name: 'Activités pour la ville de' })).toBeVisible();
+    await expect(page.getByRole('textbox', { name: 'Activité' })).toBeVisible();
+    await expect(page.getByPlaceholder('Prix')).toBeVisible();
+    await expect(page.getByRole('img', { name: 'random image of city' })).toBeVisible();
+    await expect(page.getByRole('button', { name: 'Voir plus' })).toBeVisible();
+
+// Voir Detail
+    await page.getByRole('button', { name: 'Voir plus' }).click();
+    await expect(page.getByRole('heading', { name: 'Randonnée dans les Alpes' })).toContainText('Randonnée dans les Alpes');
+    await expect(page.getByRole('img', { name: 'random image of city' })).toBeVisible();
+    await expect(page.getByText('Chamonix')).toBeVisible();
+    await expect(page.getByText('Chamonix80€/jLorem ipsum')).toBeVisible();
+    await expect(page.getByText('Ajouté par John Doe')).toBeVisible();
+// go to inital state
+
+    await page.getByRole('button').click();
+    await page.waitForURL('/explorer/Chamonix');
+    await page.getByRole('button').filter({ hasText: /^$/ }).click();
+    await page.waitForURL('/explorer');
+    await page.getByRole('link', { name: 'Candidator' }).click();
+    
+});
+
--- e2e/tests/deconexion.spec.ts
+++ e2e/tests/deconexion.spec.ts
@@ -1,14 +1,16 @@
 import { test, expect } from '@playwright/test';
 
 test('logout', async ({ page }) => {
+  await page.goto('/profil');
   //verification if connected 
     await page.locator('svg').first().click();;
     await expect(page.getByRole('link', { name: 'Déconnection' })).toBeVisible()
   //deconnexion process
     await page.getByRole('link', { name: 'Déconnection' }).click();
+    await page.waitForURL('/');
   //verification if deconnected
     await page.locator('svg').first().click();;
     await expect(page.getByRole('link', { name: 'Connection' })).toBeVisible()
-    await expect(page.getByRole('heading', { name: 'Accueil' })).toContainText('Accueil')
+    await expect(page.getByRole('heading', { name: 'Accueil' })).toBeVisible()
 
 });
